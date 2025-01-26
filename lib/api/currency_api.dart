6d52a63f66495c9aa8cd53f31857b80eb0d1ede3
snds112 <se.m.soundous@gmail.com>
2025-01-26 19:59:10 +0100

    first commit

--- lib/api/currency_api.dart
+++
@@ -1,5 +0,0 @@
-import 'package:http/http.dart' as http;
-
-Future<http.Response> fetchAlbum() {
-  return http.get(Uri.parse('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json'));
-}
